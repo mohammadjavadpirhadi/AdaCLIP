dd5ec02771d1a40e1183a6621e79e3a534020d6d
Mohammad Javad Pirhadi <30388959+mohammadjavadpirhadi@users.noreply.github.com>
2024-12-09 10:20:58 +0330

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,15 +5,15 @@ gpu_id=0
 # on the validation set as the final model.
 
 # pre-trained on MVtec and colondb
-CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig True --training_data mvtec colondb --testing_data visa
+CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig False --training_data mvtec --testing_data visa
 
-# pre-trained on Visa and Clinicdb
-CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig True --training_data visa clinicdb --testing_data mvtec
+# # pre-trained on Visa and Clinicdb
+# CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig True --training_data visa clinicdb --testing_data mvtec
 
-# This model is pre-trained on all available data to create a powerful Zero-Shot Anomaly Detection (ZSAD) model for demonstration purposes.
-CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig True \
---training_data \
-br35h brain_mri btad clinicdb colondb \
-dagm dtd headct isic mpdd mvtec sdd tn3k visa \
---testing_data mvtec
+# # This model is pre-trained on all available data to create a powerful Zero-Shot Anomaly Detection (ZSAD) model for demonstration purposes.
+# CUDA_VISIBLE_DEVICES=$gpu_id python train.py --save_fig True \
+# --training_data \
+# br35h brain_mri btad clinicdb colondb \
+# dagm dtd headct isic mpdd mvtec sdd tn3k visa \
+# --testing_data mvtec
 
